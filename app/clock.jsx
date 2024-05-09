4b681105ee43fb412bbd577f6d131161cbcf415b
imdeveloper100 <imdeveloper100@gmail.com>
2024-05-09 15:47:35 +0500

    Initial commit from Create Next App

---
+++ app/clock.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Clock() {
+  return (
+    <div>
+        <div className='bg-gray-100 min-w-[200px] min-h-[200px]'>
+            Hello
+        </div>
+    </div>
+  )
+}
+
+export default Clock
